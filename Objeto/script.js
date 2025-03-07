cef152868ef1f5dd8e6bed411e169c4f21559f8d
IsraellSan7os <ysraellsanosss@gmail.com>
2025-03-06 23:12:25 -0300

    Iniciando os estudos com Objetos
    23/03/2025

---
+++ Objeto/script.js
@@ -0,0 +1,21 @@
+let celular = 'lg'
+let marca = 'samsung'
+let modelo = 'a50'
+let memoria = '128gb'
+let color = 'negro'
+
+const MarcaCelular = {
+    marca: 'samsung',
+    modelo: 'a50',
+    memoria: '128gb',
+    color: 'negro',
+  
+    ligar: () =>{
+        console.log('logado com sucesso')
+    }
+    
+}
+
+celular.marca = 30
+console.log(celular.marca)
+celular.ligar()
