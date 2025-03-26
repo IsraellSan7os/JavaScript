9d7a4da19f742cb6c5f6dcaf0750fea13887272b
IsraellSan7os <ysraellsanosss@gmail.com>
2025-03-25 21:59:12 -0300

    Logica de progamação
    Udemy, Lopes, funções

--- Udemy/Estrutura-Repeticao.js
+++ Udemy/Estrutura-Repeticao.js
@@ -1,9 +1,9 @@
 //While ele executa primeiro e depois verifica
 
-var i = 1 
+var i = 1
 
 do {
-    console.log("Numero " +i)
+    console.log("Numero " + i)
     i++
 } while (i <= 10);
 
@@ -12,8 +12,8 @@ let num = 100
 
 do {
     console.log(num)
-    num-=2
-}while(num > 1)
+    num -= 2
+} while (num > 1)
 
 var parImpa = 100
 
@@ -22,15 +22,15 @@ do {
     if (par % 2 == 0) {
         console.log("Numero par " + par)
     } else {
-        console.log("Numero impar " + par)  
+        console.log("Numero impar " + par)
     }
-    par-= 1
-} while (par >2);
+    par -= 1
+} while (par > 2);
 
 // So numero pares Numero so pares
 
 
-var par = 100 
+var par = 100
 
 do {
     if (par % 2 == 0) {
@@ -39,8 +39,8 @@ do {
         console.log("Numero impar " + par)
     }
 
-    par -=2
-} while (par >1);
+    par -= 2
+} while (par > 1);
 
 // Numero pares e impares
 
@@ -53,7 +53,7 @@ do {
         console.log("Numero par " + impar)
     }
     impar += 2
-} while (impar <=100);
+} while (impar <= 100);
 
 /*Se o número for múltiplo de 3, exiba "Fizz" no console.
 
@@ -61,7 +61,7 @@ Se for múltiplo de 5, exiba "Buzz".
 
 Se for múltiplo de ambos (3 e 5), exiba "FizzBuzz".
 
-Caso contrário, exiba apenas o número.*/ 
+Caso contrário, exiba apenas o número.*/
 
 var multiplos = 100
 
@@ -69,12 +69,42 @@ var multiplos = 100
 do {
     if (multiplos % 3 == 0 && multiplos % 5 == 0) {
         console.log("FizzBuzz" + multiplos)
-    } else if(multiplos % 3 == 0){
+    } else if (multiplos % 3 == 0) {
         console.log('Fizz' + multiplos)
-    }else if(multiplos % 5 == 0){
+    } else if (multiplos % 5 == 0) {
         console.log('Buzz' + multiplos)
     } else {
-        console.log("Não multiplos "+multiplos)
+        console.log("Não multiplos " + multiplos)
     }
     multiplos--
 } while (multiplos > 0);
+
+
+// for 
+
+for (var numero = 1; numero < 10; numero++) {
+    console.log("Numero " + numero)
+}
+
+for (let decre = 100; decre >= 0; decre -= 3) {
+    console.log("Numero " + decre)
+}
+
+var numeor = 10
+
+res = (numero + 2) / 2
+console.log("Resultado da soma mais a divisão pro 2 é " + res)
+
+for (res = res; res >= 1; res--) {
+    console.log(res)
+}
+var i = 100
+
+for (i = 100; i > 1; i--) {
+    if (i == 5 || i == 25 || i == 35 || i == 45 || i == 55 || i == 65 || i == 75 || i == 85 || i == 95) {
+        console.log("Todos o numero que temina com 5 " + i)
+    } else {
+        console.log("Não é multiplo de 5 " + i)
+    }
+
+}
---
+++ Udemy/Funcoes.js
@@ -0,0 +1,16 @@
+function EscreverNome(nome){
+    console.log(`Meu nome é ${nome}`)
+    return nome
+}
+
+EscreverNome('Israel')
+
+function Soma(num){
+    return num
+}
+num1 = 100
+num2 = 10
+
+num = (num1 + num1) / num2
+
+console.log(Soma('Numero ' + num))
