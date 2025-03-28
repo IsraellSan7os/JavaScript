06f1b91158072d3751c4cbac393d8d85c4cd6f61
Israel Santos <ysraellsanosss@gmail.com>
2025-03-28 17:50:55 -0300

    Udemy
    Function execicios

--- Udemy/Funcoes.js
+++ Udemy/Funcoes.js
@@ -1,3 +1,7 @@
+
+
+/*
+
 function EscreverNome(nome){
     console.log(`Meu nome é ${nome}`)
     return nome
@@ -27,3 +31,40 @@ function NomeCompleto(nome, sobrenome){
 }
 
 NomeCompleto('Israel', 'Santos')
+
+
+function calcularDiaria(){
+    return 1500/22
+}
+
+var diaria = calcularDiaria()
+console.log('A diaria de um funcionario é '+ diaria)
+*/
+
+// UM funcionario recebe 1500 pro mes, quanto ele recebe por dia? e por hora?
+// O funcionario tem uma porcetagem de 3% de imposto sobre o salario bruto quanto ele recebe liquido?
+
+
+function cacularSalario(salario){
+    return salario / 30
+}
+
+var salario = cacularSalario(1500)
+console.log(`A diaria de um funcionario é R$${salario}.00 Reais por dia`)
+
+let hora = salario / 8.45
+console.log(`A hora trabalhada do funcionario é ${hora}`)
+
+let imposto = salario * 0.03
+let salarioLiquido = salario - imposto
+console.log(`O salario liquido do funcionario é ${salarioLiquido}`)
+
+
+// function com parametros
+
+function valorOvos(quantidade){
+    return quantidade / 1.5
+}
+
+let valor = valorOvos(12)
+console.log(`O valor dos ovos é ${valor}`)
