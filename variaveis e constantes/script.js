a37cbf93427dc806281ddab83696f737b254b81b
Israel Santos <ysraellsanosss@gmail.com>
2026-01-13 20:55:04 -0300

    Variavel var

---
+++ variaveis e constantes/script.js
@@ -0,0 +1,14 @@
+// Variavel sem valor
+var user
+console.log(user)
+
+// Declarar uma variável com valor
+var email = "ysrallsantos@mail.com"
+
+// Declara uma variael e depois que for atribuida, eu posso substituir por outro valor
+var senha = 1234
+console.log(senha)
+
+// Substituindo o valor
+user = 147845
+console.log(user)
