912f771d95f9404ca6aa7fcda825ee038bdfb6ef
Israel Santos <ysraellsanosss@gmail.com>
2026-02-02 19:35:38 -0300

    Falsy e Truthy

--- Estutura de condição/script.js
+++ Estutura de condição/script.js
@@ -2,3 +2,29 @@
 
 let age = 18
 console.log(age >= 18 ? "Você pode dirigir." : "Você não pode dirigir")
+
+
+//Falsy e Truthy
+
+console.log("### Exemplos de falsey ###")
+console.log(false ? "Verdadeiro" : "Falso")
+console.log(0 ? "Verdadeiro" : "Falso")
+console.log(-0 ? "Verdadeiro" : "Falso")
+console.log("" ? "Verdadeiro" : "Falso")
+console.log(null ? "Verdadeiro" : "Falso")
+console.log(undefined ? "Verdadeiro" : "Falso")
+console.log(NaN ? "Verdadeiro" : "Falso")
+
+
+console.log("### Exemplos de truthy ###")
+console.log(true ? "Verdadeiro" : "Falso")
+console.log(1 ? "Verdadeiro" : "Falso")
+console.log("0" ? "Verdadeiro" : "Falso")
+console.log("false" ? "Verdadeiro" : "Falso")
+console.log({} ? "Verdadeiro" : "Falso")
+console.log([] ? "Verdadeiro" : "Falso")
+console.log(" " ? "Verdadeiro" : "Falso")
+console.log("Israel" ? "Verdadeiro" : "Falso")
+console.log(-1 ? "Verdadeiro" : "Falso")
+console.log(Infinity ? "Verdadeiro" : "Falso")
+console.log(-Infinity ? "Verdadeiro" : "Falso")
