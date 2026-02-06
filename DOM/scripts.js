0f8a0c88da72b38d233c1601ccd44a6b91747c20
Israel Santos <ysraellsanosss@gmail.com>
2026-02-05 21:00:53 -0300

    Selecionando elementos da Dom , e alterando conteudo

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,41 +1,58 @@
-// Como visualizar os comtéudos do document.
+const selecionandoELementos = () => {
+    // Como visualizar os comtéudos do document.
 
-console.log(document)
+    console.log(document)
 
-// Como viaualizar o título da página.
-console.log(document.title)
+    // Como viaualizar o título da página.
+    console.log(document.title)
 
-// Como assesar os elementos pelo ID.
-const guest = document.getElementById("guest-2")
-console.log(guest)
+    // Como assesar os elementos pelo ID.
+    const guest = document.getElementById("guest-2")
+    console.log(guest)
 
-// Como ver as propriedade do objeto
+    // Como ver as propriedade do objeto
 
-const guest1 = document.getElementById("guest-1")
-console.dir(guest1)
+    const guest1 = document.getElementById("guest-1")
+    console.dir(guest1)
 
-// Acessa o elemento pelo className
-const guestsByClass = document.getElementsByClassName("guest")
-console.log(guestsByClass)
+    // Acessa o elemento pelo className
+    const guestsByClass = document.getElementsByClassName("guest")
+    console.log(guestsByClass)
 
-// Como exibir o elemento espesifico da lista selecionada.
-// A posição começa do 0.
-console.log(guestsByClass[0])
-console.log(guestsByClass.item(1))
+    // Como exibir o elemento espesifico da lista selecionada.
+    // A posição começa do 0.
+    console.log(guestsByClass[0])
+    console.log(guestsByClass.item(1))
 
-// Acessa o elemento pelo nome da tag.
-const guestsByTag = document.getElementsByTagName("li")
-console.log(guestsByTag)
+    // Acessa o elemento pelo nome da tag.
+    const guestsByTag = document.getElementsByTagName("li")
+    console.log(guestsByTag)
 
 
-// Acessa o elemento pelo querySelector (seleciona o primeiro elemento que encontrar).
-// So pega o primeiro elemento que encontrar.
-const firstGuest = document.querySelector(".guest")
-console.log("Query", firstGuest)
+    // Acessa o elemento pelo querySelector (seleciona o primeiro elemento que encontrar).
+    // So pega o primeiro elemento que encontrar.
+    const firstGuest = document.querySelector(".guest")
+    console.log("Query", firstGuest)
 
-const firstGuestById = document.querySelector("#guest-1")
-console.log("Query by ID", firstGuestById)
+    const firstGuestById = document.querySelector("#guest-1")
+    console.log("Query by ID", firstGuestById)
 
-// Como faz pra retornar todos os elementos, com o querySelectorAll.
-const allGuests = document.querySelectorAll(".guest")
-console.log("All Guests", allGuests)
+    // Como faz pra retornar todos os elementos, com o querySelectorAll.
+    const allGuests = document.querySelectorAll(".guest")
+    console.log("All Guests", allGuests)
+}
+
+const manipulandoElementos = () => {
+    const guest = document.querySelector("#guest-1")
+    
+    // Retorna o conteúdo que esta no html
+    console.log(guest.textContent)
+    // Altera o conteúdo da spam 
+    guest.textContent = "Anne"
+
+    console.log(guest.textContent) //Conteúdo visivel e oculto
+    console.log(guest.innerText) // Retorna somente o conteudo visível
+    console.log(guest.innerHTML)  //Retorna o Html como texto
+}
+
+manipulandoElementos()
