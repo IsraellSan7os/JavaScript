183fad04089c483b29abc0c73eda5c41e351154e
Israel Santos <ysraellsanosss@gmail.com>
2026-03-03 20:06:53 -0300

    Eventos de formulário

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -125,4 +125,14 @@ const event = () => {
     })
 }
 
-event()
+const eventosDeFormulario = () => {
+    const form = document.querySelector("form")
+
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault()
+
+        console.log("Você fez um submit")
+    })
+}
+
+eventosDeFormulario()
