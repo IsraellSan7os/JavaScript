ba86cd3093a4a3dc81a78dfb87d0b32824309d6e
Israel Santos <ysraellsanosss@gmail.com>
2026-04-11 23:24:58 -0300

    Estudos de manipulação de strings em JavaScript
    * Uso de `split()` para dividir strings em arrays
    * Uso de `join()` para juntar arrays em strings
    * Aplicação de `padStart()` e `padEnd()` para preenchimento de strings
    * Uso de `slice()` para extrair partes de strings

--- JavaScript-intermediario/Métodos de texto/scritp.js
+++ JavaScript-intermediario/Métodos de texto/scritp.js
@@ -51,3 +51,69 @@ function SubstituindoFatiandoTexto() {
 }
 
 SubstituindoFatiandoTexto();
+
+function completandoUmaString() {
+  const crediCard = "12345678910111213141516";
+  console.log(crediCard);
+
+  // Ultimos 4 numero
+  let lasDigits = crediCard.slice(-4);
+  console.log(lasDigits);
+
+  // padStar preeche a string do início.
+  const res = lasDigits.padStart(crediCard.length, "x");
+  console.log(res);
+
+  // padSEnd preeche a string do final.
+  let number = "123425";
+  console.log(number.padEnd(11, "#"));
+}
+
+completandoUmaString();
+
+// Separando
+function separandoUnindoStrings() {
+  let text = "Estudar,aprender,praticar";
+  console.log(text);
+
+  let separete = text.split(",");
+  console.log(separete);
+
+  let joined = separete.join(" - ");
+  console.log(joined);
+
+  let cep = "15520,000 000,000";
+  console.log(cep);
+  let r = cep.split(",");
+  let i = r.join(" - ");
+  console.log(i);
+
+  console.log("************************************");
+}
+
+separandoUnindoStrings();
+function cartao() {
+  let user = {
+    name: "Isrel",
+    lastName: "Santos",
+  };
+  let number = {
+    numero: "12334,12345",
+    date: 20 / 30,
+    cvc: 230,
+  };
+
+  console.log(`Nome: ${user.name}`);
+  console.log(`Sobrenome: ${user.lastName}`);
+  let j = user.name + user.lastName;
+  console.log(j);
+
+  let num = number.numero;
+  let r = num.split(",");
+  let join = r.join(" - ");
+  console.log(join);
+  let restt = join.padEnd(15, "#")
+ 
+  console.log(`Cartão:`, restt);
+}
+cartao();
