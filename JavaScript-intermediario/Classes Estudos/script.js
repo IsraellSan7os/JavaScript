0621d19a93f441d893a305f9a7165f3e1d5b6d05
Israel Santos <ysraellsanosss@gmail.com>
2026-04-28 19:50:50 -0300

    Exercício para treinar uso de classes e herança em JavaScript

--- JavaScript-intermediario/Classes Estudos/script.js
+++ JavaScript-intermediario/Classes Estudos/script.js
@@ -101,6 +101,7 @@ const heranca_class = () => {
   pastor_Alemao.showMessage();
   const vat = new Dog();
   vat.acao.babando();
+  vat.acao.latir();
 
   class cat extends Animal {
     acao = {
@@ -121,7 +122,160 @@ const heranca_class = () => {
   viralata.run();
 };
 
+const heranca_class_ex = () => {
+  class Celular {
+    constructor(tela, processador, memoria, camera, bateria) {
+      this.tela = tela;
+      this.processador = processador;
+      this.memoria = memoria;
+      this.camera = camera;
+      this.bateria = bateria;
+    }
+    showMessage() {
+      const date = new Date();
+      const formatDate = date.toLocaleDateString("pt-BR");
+      console.log(` LOJÁ XIAOME`, formatDate);
+    }
+  }
+
+  const main = new Celular();
+  main.showMessage();
+
+  class xiaome_17Info extends Celular {
+    constructor(tela, processador, memoria, camera, bateria, price) {
+      super(tela, processador, memoria, camera, bateria);
+
+      this.price = price;
+    }
+
+    ShowPrice() {
+      let price = 1200;
+      let formatAmout = price.toLocaleString("pt-Br", {
+        style: "currency",
+        currency: "BRL",
+      });
+      console.log("Preço", formatAmout);
+    }
+
+    showMessageXiaome_17() {
+      console.log("1. Xiaomi 17 (O Compacto Premium)");
+      console.log(
+        `
+      Tela: ${this.tela}
+      Processador: ${this.processador}
+      Memória: ${this.memoria}
+      Camera: ${this.camera}
+      Bateria: ${this.bateria}
+    `,
+      );
+    }
+  }
+
+  const xiaome17 = new xiaome_17Info(
+    "6.28'' AMOLED",
+    "Snapdragon 8 Gen 2",
+    "12GB RAM",
+    "50MP",
+    "4500mAh",
+  );
+  xiaome17.showMessageXiaome_17();
+  xiaome17.ShowPrice();
+
+  class Xiaomi_17Pro extends Celular {
+    constructor(tela, processador, memoria, camera, bateria, price) {
+      super(tela, processador, memoria, camera, bateria);
+    }
+
+    ShowPrice() {
+      let price = 1570.45;
+      let formatAmout = price.toLocaleString("pt-Br", {
+        style: "currency",
+        currency: "BRL",
+      });
+      console.log("Preço", formatAmout);
+    }
+
+    showMessageXiaome_17() {
+      console.log("1. Xiaomi 17 (O Compacto Premium)");
+      console.log(
+        `
+      Tela: ${this.tela}
+      Processador: ${this.processador}
+      Memória: ${this.memoria}
+      Camera: ${this.camera}
+      Bateria: ${this.bateria}
+    `,
+      );
+    }
+  }
+
+  const xiaomi17Pro = new Xiaomi_17Pro(
+    "6.73'' AMOLED 120Hz",
+    "Snapdragon 8 Gen 2",
+    "16GB RAM",
+    "108MP",
+    "5000mAh",
+  );
+  xiaomi17Pro.showMessageXiaome_17();
+  xiaomi17Pro.ShowPrice();
+
+  class Xiaomi_17Ultra extends Celular {
+    constructor(
+      tela,
+      processador,
+      memoria,
+      camera,
+      bateria,
+      resistenciaAgua,
+      carregamento,
+      price,
+    ) {
+      super(tela, processador, memoria, camera, bateria);
+      this.resistenciaAgua = resistenciaAgua;
+      this.carregamento = carregamento;
+      this.price = this.price;
+    }
+
+    ShowPrice() {
+      let price = 1999.99;
+      let formatAmout = price.toLocaleString("pt-Br", {
+        style: "currency",
+        currency: "BRL",
+      });
+      console.log("Preço", formatAmout);
+    }
+
+    showMessageXiaomi_17Ultra() {
+      console.log("2. Xiaomi 17 Ultra (O Topo de Linha)");
+      console.log(`
+      Tela: ${this.tela}
+      Processador: ${this.processador}
+      Memória: ${this.memoria}
+      Câmera: ${this.camera}
+      Bateria: ${this.bateria}
+      Resistência à água: ${this.resistenciaAgua}
+      Carregamento: ${this.carregamento}
+    `);
+    }
+  }
+
+  // Instanciando Xiaomi_17Ultra e exibindo informações do aparelho
+  const xiaomi17Ultra = new Xiaomi_17Ultra(
+    "6.81'' AMOLED 144Hz",
+    "Snapdragon 8 Gen 3",
+    "24GB RAM",
+    "200MP + 50MP + 50MP",
+    "5500mAh",
+    "IP68",
+    "120W Turbo Charge",
+  );
+  xiaomi17Ultra.showMessageXiaomi_17Ultra();
+  xiaomi17Ultra.ShowPrice();
+};
+
+// Aprendendo sobre herança e cadeia de protótipos
+
 adicionando_metodos_nas_classes();
 estatico();
 heranca_class();
-
+heranca_class_ex();
