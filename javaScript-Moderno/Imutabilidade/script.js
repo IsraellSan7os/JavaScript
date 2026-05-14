e53ee68a57e92a02d7dc3fabe05da57699e9ebc1
Israel Santos <ysraellsanosss@gmail.com>
2026-05-13 22:27:58 -0300

    Estudo sobre imutabilidade e Object.freeze no JavaScript".

--- javaScript-Moderno/Imutabilidade/script.js
+++ javaScript-Moderno/Imutabilidade/script.js
@@ -85,7 +85,7 @@ const deepCopy = function () {
   /*
   Quando fazemos desse jeito ele vai adcionar tanto
   no html quando no de js, Mais não é isso que eu quero
-  
+
   */
   jsCurse.students.push({ name: "Anne", email: "anne@gmail.com" });
   console.log(htmlCursor, jsCurse);
@@ -183,12 +183,108 @@ const stautus = {
   veterinaria: [...animal.veterinaria],
 };
 
-stautus.veterinaria.push(
-  {
-    name: "Israel",
-    lastName: "santos",
-    emial: "ysrael@gmail.com",
-  }
-);
+stautus.veterinaria.push({
+  name: "Israel",
+  lastName: "santos",
+  emial: "ysrael@gmail.com",
+});
 
 console.log(animal, stautus);
+
+console.log("_________________________________");
+const htmlCss = {
+  structure: {
+    html: "Html",
+    css: "Css",
+  },
+
+  students: [
+    {
+      show: "Alunos do curso de Html css",
+      name: "Israel",
+      email: "ysraelsantos@gmail.com",
+      password: "123456",
+    },
+  ],
+};
+
+const jsCourse = {
+  ...htmlCss,
+
+  course: {
+    modulo_0: "1",
+    modulo_1: "2",
+  },
+
+  structure: {
+    ...htmlCss.structure,
+  },
+
+  students: [...htmlCss.students],
+};
+
+jsCourse.students.push({
+  name: "Anne",
+  email: "anne@gmail.com",
+  password: "654321",
+});
+
+const full_stack = {
+  show: "Full-Stack",
+  ...htmlCss,
+  ...jsCourse,
+
+  structure: { ...htmlCss.structure },
+  students: [...jsCourse.students],
+  course: { ...jsCourse.course },
+};
+
+// adicionando depois
+full_stack.structure = {
+  js: "JavaScript",
+};
+full_stack.structure = {
+  react: "React",
+};
+
+full_stack.course.modulo_2 = {
+  modulo_0: "3",
+  modulo_1: "4",
+};
+
+console.log(full_stack, htmlCss, jsCourse);
+
+//  como congelar um objeto em JavaScript para evitar modificações
+console.log("como congelar um objeto em JavaScript para evitar modificações");
+const book = {
+  title: "Objetos Imutáveis",
+  category: "JavaScript",
+  outhor: {
+    name: "Israel",
+    email: "ysrael@gmail.com",
+  },
+};
+
+// Conjela o objeto e inpôe a modificação
+Object.freeze(book);
+book.category = "css"; //Não modifica
+
+// O Object.freezer() Não impede modificações profundas
+// em objetos aninhados (Shallow freezin)
+book.outhor.name = "joão";
+console.log(book);
+
+console.log(book);
+
+// Deep Freeze
+const bookk = {
+  title: "Objetos Imutáveis",
+  category: "JavaScript",
+  outhor: {
+    name: "Israel",
+    email: "ysrael@gmail.com",
+  },
+};
+
+
+console.log("###########################")
